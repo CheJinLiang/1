57d2faaecf7cd33b11dc9e0524795950881fdb6f
CheJinLiang <50006609+CheJinLiang@users.noreply.github.com>
2019-04-27 10:27:47 +0800

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,14 @@
+age = 5
+if age >= 18:
+    print('Your age is',age)
+    print('adult')
+elif age >= 6:
+    print('Your age is',age)
+    print('teenager')
+else:
+    print('Your age is',age)
+    print('kid')
+x = 0
+if x:
+    print('True')
+    
