475195b9da4e72a569d302e5228b9e3b64cd4daf
CheJinLiang <50006609+CheJinLiang@users.noreply.github.com>
2019-04-27 10:27:27 +0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,18 @@
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print('n =',n,
+'f =',f,
+'s1 =',s1,
+'s2 =',s2,
+'s3 =',s3,
+'s4 =',s4)
+print(
+    n,f,s1,s2,s3,s4
+)
+
